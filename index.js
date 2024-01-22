8e90a9298c96176185fcd0713dbcca4766f25836
shanmukh-sai09 <21131a1226@gvpce.ac.in>
2024-01-22 14:26:30 +0530

    Add cancle feature to message container

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const loginFormElement = document.getElementById("form-login");
 const messageText = document.querySelector(".message-text");
+const messageContainer = document.querySelector(".message-container");
+const messageCancleBtn = document.querySelector(".cancle-btn");
 const passwordRegEx = new RegExp("^[a-zA-Z0-9@_]{8,}$");
 const passwordRgx = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@_])[a-zA-Z0-9@_]{8, }");
 
@@ -11,6 +13,7 @@ loginFormElement.addEventListener("submit", (e) => {
     const username = usernameElement.value;
     const password = passwordElement.value;
 
+    messageContainer.style.visibility = "visible";
     if(username === "" || username.length < 6) {
         messageText.innerText = "* Invalid username!! username must be aleast 6 characters.";
     } else if(password.length < 8) {
@@ -22,3 +25,6 @@ loginFormElement.addEventListener("submit", (e) => {
     }
 });
 
+messageCancleBtn.addEventListener("click", () => {
+    messageContainer.style.visibility = "hidden";
+})
